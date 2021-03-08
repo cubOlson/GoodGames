81a326400fdd5c6ffb95772224644f8ab137abc7
Drew Thurman <drewthurm21@gmail.com>
2021-03-08 12:25:13 -0700

    creating seeders for Platforms & Categories

---
+++ db/seeders/20210308191640-seed-categories.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('Categories', [
+     ('Action & Adventure')
+     ('Role Playing & MMO')
+     ('Strategy & RTS')
+     ('Sports & Racing')
+     ('Casual & Simulation')
+   ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('People', null, {});
+  }
+};
