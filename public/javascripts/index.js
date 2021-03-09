cb7dbaa858604d5d2fca061b76e7e7b3d178c1b8
Eric Ramsay <eramsay10@gmail.com>
2021-03-09 10:43:12 -0500

    added login logout fix

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,26 @@
 window.addEventListener("load", (event)=>{
     console.log("hello from javascript!")
 })
+
+// window.addEventListener('DOMContentLoaded', e => {
+//     const loginButton = document.querySelector('#login-button');
+//     const logoutButton = document.querySelector('#logout-button');
+
+//     loginButton.addEventListener('click', e => {
+//         console.log('login clicked');
+
+//         setTimeout(()=> {
+//             console.log('timeout triggered...');
+//             window.location.href = 'http://localhost:8080/'
+//         }, 1000);
+//     })
+
+//     logoutButton.addEventListener('click', e => {
+//          console.log('logout clicked');
+//         setTimeout(()=> {
+//             console.log('timeout triggered...');
+//             window.location.href = 'http://localhost:8080/'
+//         }, 1000);
+//     })
+
+// });
