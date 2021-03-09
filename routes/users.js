ca7d9543512a07ec73f6bddc374831760d473e16
Eric Ramsay <eramsay10@gmail.com>
2021-03-09 12:42:25 -0500

    added login and registration validation mw functions

--- routes/users.js
+++ routes/users.js
@@ -1,76 +1,64 @@
 const express = require("express");
 const router = express.Router();
 const { loginUser, logoutUser } = require('../auth')
-
 const { User } = require('../db/models/');
+const { asyncHandler, csrfProtection, bcrypt, check, validationResult, loginValidation, registerValidation } = require("./utils");
 
-const {
-  asyncHandler,
-  csrfProtection,
-  bcrypt,
-  check,
-  validationResult,
-} = require("./utils");
-
-/* GET users listing. */
+/* GET */
 router.get("/login", csrfProtection, asyncHandler(async(req, res, next) => {
-    res.render("user-login", { csrfToken: req.csrfToken(), title: "Login" });
+    // const user = await User.build();
+    // // 'my username', 'my first name','my last name','206-555-2165','myemail@gmail.com'
+    res.render("user-login", { csrfToken: req.csrfToken(), title: "Login", user });
   })
 );
 
-router.get(
-  "/signup",
-  csrfProtection,
-  asyncHandler(async (req, res, next) => {
+router.get( "/signup", csrfProtection, asyncHandler(async (req, res, next) => {
     res.render("user-signup", { csrfToken: req.csrfToken(), title: "Sign Up" });
   })
 );
 
-router.post('/signup', csrfProtection, asyncHandler( async(req, res, next) => {
 
-  const {
-    username,
-    firstName,
-    lastName,
-    phone,
-    email,
-    password,
-    confirmPassword
-  } = req.body;
 
-  const newUser = await User.build({
-    username,
-    firstName,
-    lastName,
-    phone,
-    email
-  });
+/* POST */
 
-  let hashedPassword = await bcrypt.hash(password, 10);
-  newUser.hashedPassword = hashedPassword;
-  await newUser.save();
-  loginUser(req, res, newUser);
-  res.redirect('/');
+// Register New User
+router.post('/signup', csrfProtection, registerValidation, asyncHandler( async(req, res, next) => {
+  const { username, firstName,lastName, phone, email, password, confirmPassword } = req.body;
+
+  const validationErrors = validationResult(req);
+
+  if(validationErrors.isEmpty()) {
+    const user = await User.build({ username, firstName, lastName, phone, email});
+
+    let hashedPassword = await bcrypt.hash(password, 10);
+    user.hashedPassword = hashedPassword;
+    await user.save();
+    loginUser(req, res, user);
+    return
+
+  } else {
+    const errors = validationErrors.array().map(error => error.msg);
+    console.log(errors);
+    res.render('user-signup', { errors, csrfToken: req.csrfToken(), title: "Login", } )
+  }
 }));
 
-router.post('/login', csrfProtection, asyncHandler(async(req, res) => {
+
+// Register New User
+router.post('/login', csrfProtection, loginValidation, asyncHandler(async(req, res) => {
   const { email, password } = req.body;
 
   //Find the user based on credentials (req)
-  const user = await User.findOne({
-    where: {
-      email
-    }
-  });
+  const user = await User.findOne({ where: { email } });
   //if user found, compare their password to hashed password (bcrypt)
   if (user) {
     const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
 
-  //if passwords match, log in the user, redirect to root
+    //if passwords match, log in the user, redirect to root
     if (passwordMatch) {
       loginUser(req, res, user);
       return
-  }
+    }
 }
 
   // res.render('user-login', { title: 'Login', email, csrfToken: req.csrfToken() });
--- routes/utils.js
+++ routes/utils.js
@@ -9,10 +9,48 @@ const asyncHandler = (handler) => (req, res, next) => {
     return handler(req, res, next).catch(next)
 }
 
+// email password validation
+const loginValidation = [
+    check('email')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a valid email.'),
+    check('password')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a valid password.'),
+]
+
+// registration validation
+const registerValidation = [
+    check('firstName')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a valid first name.'),
+    check('lastName')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a valid last name.'),
+    check('username')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a valid username.'),
+    check('email')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a valid email.'),
+    check('password')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a valid password.')
+        .isLength({ min: 4}),
+        // .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{4,}$/, "i")
+        // .withMessage('Password must have one digit, one lower-case, one upper-case, and one number.'),
+        // must have one digit, one lower-case, one upper-case, one number ... min 4 chars
+    check('confirmPassword')
+        .matches('password')
+        .withMessage('Passwords must match.'),
+]
+
 module.exports = {
     asyncHandler,
     csrfProtection,
     bcrypt,
     check,
-    validationResult
+    validationResult,
+    loginValidation,
+    registerValidation,
 }
