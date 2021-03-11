7ec073f5c43093462cd259004793c1dd688bed76
Drew Thurman <drewthurm21@gmail.com>
2021-03-11 15:16:29 -0700

    adding changes to review routes

--- routes/games.js
+++ routes/games.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const router = express.Router()
-const { asyncHandler, sanityCheck } = require('./utils')
-const { Game, Category } = require('../db/models')
+const { asyncHandler, sanityCheck, csrfProtection } = require('./utils')
+const { Game, Category, User, Review } = require('../db/models')
 
 /* GET */
-router.get('/:id', asyncHandler( async (req, res) => {
+router.get('/:id', csrfProtection, asyncHandler( async (req, res) => {
   const gameId = parseInt(req.params.id, 10)
-  console.log(gameId)
   const game = await Game.findByPk(gameId, {include: Category})
-  console.log(game)
-  res.render('game-page', { title: `${game.title}`, game })
+  const reviews = await Review.findAll({where: {gameId}, limit:10})
+
+  res.render('game-page', { title: `${game.title}`, game, reviews, csrfToken: req.csrfToken() })
 }));
 
 
 
+router.post('/:id/review', csrfProtection, asyncHandler(async (req, res) => {
+  const { title, content, liked} = req.body
+  const gameId = parseInt(req.params.id, 10)
+
+  const newReview = await Review.create({
+    gameId, 
+    userId: req.session.auth,
+    title,
+    content,
+    liked: liked
+  })
+
+  res.redirect(`http://localhost:8080/games/${gameId}`)
+}))
 
 module.exports = router
