4f36e32b3f72e5695646a8e6622ece65fa3fcc3f
Drew Thurman <drewthurm21@gmail.com>
2021-03-12 12:41:23 -0700

    added demo user functionality

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,16 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
-})
+document.addEventListener('DOMContentLoaded', event => {
 
-const demoUserBtn = document.getElementById('demoUserBtn')
-const loginEmail = document.getElementById('')
+    window.addEventListener("load", (event)=>{
+        console.log("hello from javascript!")
+    })
+    
+    const demoUserBtn = document.getElementById('demoUserBtn')
+    const email = document.getElementById('login-email')
+    const password = document.getElementById('login-password')
+    
+    demoUserBtn.addEventListener('click', e =>  {
+        e.preventDefault()
+        email.value = 'demo1@gmail.com'
+        password.value = '1!aA'
+    })                             
+})
