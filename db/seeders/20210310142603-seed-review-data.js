27d40d3037faaa04fdc2fd8c71b3c304a9daa92a
cubOlson <cub.olson@gmail.com>
2021-03-12 12:25:37 -0500

    First

--- db/seeders/20210310142603-seed-review-data.js
+++ db/seeders/20210310142603-seed-review-data.js
@@ -7,6 +7,18 @@ module.exports = {
       {gameId:1, userId:2, title:"Not bad, but would wait for sale", content:`After putting a solid weekend into the main story, I would say its overall pretty good. But if I were to do it again, I'd wait for a killer sale. Not worth buying at retail.`, liked: true, createdAt: new Date(), updatedAt: new Date()},
       {gameId:1, userId:3, title:"Awful", content:`Sucked. What more is there to say? The total play time for the main story line is only ten hours! What a waste of money. Didn't even last me a Saturday. Terrible voice acting. Bugs on bugs. I want my money back.`, liked: false, createdAt: new Date(), updatedAt: new Date()},
       {gameId:1, userId:4, title:"Meh", content:`Not great, not terrible.`, liked: true, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:2, userId:1, title:"I just loved it.", content:`Seriously, this is one of the best games I've ever played. So worth the money!`, liked: true, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:2, userId:2, title:"Not bad, but would wait for sale", content:`After putting a solid weekend into the main story, I would say its overall pretty good. But if I were to do it again, I'd wait for a killer sale. Not worth buying at retail.`, liked: true, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:2, userId:3, title:"Awful", content:`Sucked. What more is there to say? The total play time for the main story line is only ten hours! What a waste of money. Didn't even last me a Saturday. Terrible voice acting. Bugs on bugs. I want my money back.`, liked: false, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:2, userId:4, title:"Meh", content:`Not great, not terrible.`, liked: true, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:3, userId:1, title:"Wow, what a game!", content:`Seriously, this is one of the best games I've ever played. So worth the money!`, liked: true, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:3, userId:2, title:"Not bad, but would wait for sale", content:`After putting a solid weekend into the main story, I would say its overall pretty good. But if I were to do it again, I'd wait for a killer sale. Not worth buying at retail.`, liked: true, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:3, userId:3, title:"Awful", content:`Sucked. What more is there to say? The total play time for the main story line is only ten hours! What a waste of money. Didn't even last me a Saturday. Terrible voice acting. Bugs on bugs. I want my money back.`, liked: false, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:3, userId:4, title:"Meh", content:`Not great, not terrible.`, liked: true, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:4, userId:1, title:"I just loved it.", content:`Seriously, this is one of the best games I've ever played. So worth the money!`, liked: true, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:4, userId:2, title:"Not bad, but would wait for sale", content:`After putting a solid weekend into the main story, I would say its overall pretty good. But if I were to do it again, I'd wait for a killer sale. Not worth buying at retail.`, liked: true, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:4, userId:3, title:"Awful", content:`Sucked. What more is there to say? The total play time for the main story line is only ten hours! What a waste of money. Didn't even last me a Saturday. Terrible voice acting. Bugs on bugs. I want my money back.`, liked: false, createdAt: new Date(), updatedAt: new Date()},
+      {gameId:4, userId:4, title:"Meh", content:`Not great, not terrible.`, liked: true, createdAt: new Date(), updatedAt: new Date()},
       ], {});
   },
 
