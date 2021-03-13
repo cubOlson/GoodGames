29dd2da8cc9842558c5fbcab101dd010ad261501
Eric Ramsay <eramsay10@gmail.com>
2021-03-12 19:18:56 -0500

    added updated cards to mygames/myreviews/search

--- routes/search.js
+++ routes/search.js
@@ -7,6 +7,7 @@ const { Op } = require('sequelize');
 const router = express.Router();
 
 router.get('/:search', asyncHandler(async (req, res) => {
+
     const searching = await db.Game.findAll({
         where: {
             [Op.or]: {
@@ -16,7 +17,18 @@ router.get('/:search', asyncHandler(async (req, res) => {
             },
         },
     });
-    res.render('search', {searching});
+    if(req.session.auth){
+        const { userId } = req.session.auth
+        const records = await db.UserGame.findAll( {where: { userId }})
+        const gameStatuses = {};
+        records.forEach( record => {
+            const { gameId, userId, status, reviewed } = record;
+            gameStatuses[gameId] = status // add key/value to gameStatuses obj for mixin
+        })
+        res.render('search', {searching, gameStatuses});
+    } else {
+        res.render('search', {searching});
+    }
 }))
 
 module.exports = router;
